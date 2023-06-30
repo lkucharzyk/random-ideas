0e7c5ed98494ea50bc8b39034e68c93ee84f5ee9
Łukasze <lukaszkucharzyk17@gmail.com>
2023-06-30 09:47:57 +0200

    added post request handling

---
+++ routes/ideas.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+
+const ideas = [
+    {
+      id: 1,
+      text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+      tag: 'Technology',
+      username: 'TonyStark',
+      date: '2022-01-02',
+    },
+    {
+      id: 2,
+      text: 'Milk cartons that turn a different color the older that your milk is getting',
+      tag: 'Inventions',
+      username: 'SteveRogers',
+      date: '2022-01-02',
+    },
+    {
+      id: 3,
+      text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+      tag: 'Software',
+      username: 'BruceBanner',
+      date: '2022-01-02',
+    },
+  ];
+
+  router.get('/', (req, res)=>{
+    res.json({sucess: true, data: ideas})
+})
+
+router.get('/:id', (req, res)=>{
+    const idea = ideas.find(idea => idea.id === +req.params.id)
+
+    if(!idea){
+      return  res.status(404).json({sucess:false, error: 'resource not found'})
+    }
+    
+    res.json({sucess: true, data: idea})
+})
+
+//add idea
+
+router.post('/', (req, res) =>{
+    const idea = {
+        id: ideas.length + 1,
+        text: req.body.text,
+        tag : req.body.tag,
+        username: req.body.username,
+        date: new Date().toISOString().slice(0, 10)
+    }
+
+    ideas.push(idea)
+
+    res.json({sucess: true, data: idea})
+})
+
+module.exports = router
